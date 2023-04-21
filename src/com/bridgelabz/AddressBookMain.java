46701cb1d94a69423d183c060600677a5fc56cbf
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-04-22 02:08:03 +0530

    Added:Welcome To Address Book System Create Contact In Address Book

---
+++ src/com/bridgelabz/AddressBookMain.java
@@ -0,0 +1,41 @@
+package com.bridgelabz;
+
+import java.util.Scanner;
+
+public class AddressBookMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        // prompt the user for input
+        System.out.println("Enter first name:");
+        String firstName = scanner.nextLine();
+
+        System.out.println("Enter last name:");
+        String lastName = scanner.nextLine();
+
+        System.out.println("Enter address:");
+        String address = scanner.nextLine();
+
+        System.out.println("Enter city:");
+        String city = scanner.nextLine();
+
+        System.out.println("Enter state:");
+        String state = scanner.nextLine();
+
+        System.out.println("Enter zip:");
+        String zip = scanner.nextLine();
+
+        System.out.println("Enter phone number:");
+        String phoneNumber = scanner.nextLine();
+
+        System.out.println("Enter email:");
+        String email = scanner.nextLine();
+
+        // create a new Contact instance with the user's input
+        Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
+
+        // print the new contact
+        System.out.println("New contact created:");
+        System.out.println(newContact);
+    }
+}
