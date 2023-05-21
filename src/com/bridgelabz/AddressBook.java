89b7bb767d929e1c613e8fb9170eb7c1eb453c96
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-21 15:41:36 +0530

    Added:Edit contact in address book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,22 +1,84 @@
 package com.bridgelabz;
 import java.util.ArrayList;
+import java.util.Scanner;
+
 public class AddressBook {
-        // Declare an instance variable called 'contacts' that holds an ArrayList of Contact objects
-        private ArrayList<Contact> contacts;
+    ArrayList<Contacts> contactDetails = new ArrayList();
+    Scanner sc = new Scanner(System.in);
 
-        // Create a constructor for AddressBook that initializes the 'contacts' ArrayList
-        public AddressBook() {
-            this.contacts = new ArrayList<Contact>();
-        }
+    public void addDetails() {
+        Contacts info = new Contacts();
+        System.out.println("Enter first name: ");
+        info.setFirstName(sc.nextLine());
+        System.out.println("Enter last name: ");
+        info.setLastName(sc.nextLine());
+        System.out.println("Enter address: ");
+        info.setAddress(sc.nextLine());
+        System.out.println("Enter city name: ");
+        info.setCity(sc.nextLine());
+        System.out.println("Enter state name: ");
+        info.setState(sc.nextLine());
+        System.out.println("Enter zip code: ");
+        info.setZipCode(sc.nextLine());
+        System.out.println("Enter contact no.: ");
+        info.setContactNo(sc.nextLine());
+        System.out.println("Enter email: ");
+        info.setEmail(sc.nextLine());
+        contactDetails.add(info);
+    }
 
-        // Define a method called 'addContact' that takes a Contact object as a parameter
-        // and adds it to the 'contacts' ArrayList
-        public void addContact(Contact contact) {
-            this.contacts.add(contact);
-        }
+    public void displayContacts() {
+        System.out.println(contactDetails);
+    }
 
-        // Define a method called 'getContacts' that returns the 'contacts' ArrayList
-        public ArrayList<Contact> getContacts() {
-            return this.contacts;
+    public void editDetails(String searchFirstName) {
+        for (Contacts contact : contactDetails) {
+            System.out.println("First name: " + contact.getFirstName());
+            if (contact.getFirstName().equals(searchFirstName)) {
+                System.out.println("Enter the number to edit respective info: ");
+                System.out.println("1. First Name \n2. Last Name \n3. Address \n4. City " +
+                        "\n5. State \n6. Zip Code \n7. Contact No \n8. Email");
+                int index = sc.nextInt();
+                System.out.println("Enter value to update: ");
+                switch (index) {
+                    case 1:
+                        String updatedFirstName = sc.next();
+                        contact.setFirstName(updatedFirstName);
+                        break;
+                    case 2:
+                        String updatedLastName = sc.next();
+                        contact.setLastName(updatedLastName);
+                        break;
+                    case 3:
+                        String updatedAddress = sc.next();
+                        contact.setAddress(updatedAddress);
+                        break;
+                    case 4:
+                        String updatedCity = sc.next();
+                        contact.setCity(updatedCity);
+                        break;
+                    case 5:
+                        String updatedState = sc.next();
+                        contact.setState(updatedState);
+                        break;
+                    case 6:
+                        String updatedZipCode = sc.next();
+                        contact.setZipCode(updatedZipCode);
+                        break;
+                    case 7:
+                        String updatedContact = sc.next();
+                        contact.setContactNo(updatedContact);
+                        break;
+                    case 8:
+                        String updatedEmail = sc.next();
+                        contact.setEmail(updatedEmail);
+                        break;
+                    default:
+                        System.out.println("Invalid number!");
+                }
+            } else {
+                System.out.println("No record found!");
+            }
         }
     }
+}
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -2,47 +2,14 @@ package com.bridgelabz;
 import java.util.Scanner;
 public class AddressBookMain {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        // prompt the user for input
-        System.out.println("Enter first name:");
-        String firstName = scanner.nextLine();
-
-        System.out.println("Enter last name:");
-        String lastName = scanner.nextLine();
-
-        System.out.println("Enter address:");
-        String address = scanner.nextLine();
-
-        System.out.println("Enter city:");
-        String city = scanner.nextLine();
-
-        System.out.println("Enter state:");
-        String state = scanner.nextLine();
-
-        System.out.println("Enter zip:");
-        String zip = scanner.nextLine();
-
-        System.out.println("Enter phone number:");
-        String phoneNumber = scanner.nextLine();
-
-        System.out.println("Enter email:");
-        String email = scanner.nextLine();
-
-        // create a new Contact instance with the user's input
-        Contact newContact = new Contact(firstName, lastName, address, city, state, zip, phoneNumber, email);
-
-        // print the new contact
-        System.out.println("New contact created:");
-        System.out.println(newContact);
-        // create a new address book
-        AddressBook addressBook = new AddressBook();
-        // add the new contact to the address book
-        addressBook.addContact(newContact);
-
-        // print out the current list of contacts in the address book
-        System.out.println("Current contacts in address book:");
-        for (Contact contact : addressBook.getContacts()) {
-            System.out.println(contact);
-        }
+        Scanner sc = new Scanner(System.in);
+        AddressBook addressOfPerson1 = new AddressBook();
+        addressOfPerson1.addDetails();
+        addressOfPerson1.displayContacts();
+        System.out.println("Enter First Name for which you want to modify info: ");
+        String fName = sc.nextLine();
+        addressOfPerson1.editDetails(fName);
+        System.out.println("Updated Details: ");
+        addressOfPerson1.displayContacts();
     }
 }
