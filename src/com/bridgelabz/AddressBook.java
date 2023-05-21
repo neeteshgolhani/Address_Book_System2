4868a54e8d7c9787ccf9913c9d627f950e9a9b1f
neeteshgolhani <neeteshgolhani4@gmail.com>
2023-05-21 16:28:01 +0530

    Added:Ability to add multiple  address book

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -1,12 +1,51 @@
 package com.bridgelabz;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class AddressBook {
     List<Contacts> contactDetails = new ArrayList<>(); // List to store contact details
     Scanner sc = new Scanner(System.in); // Scanner for user input
+    Map<String, AddressBook> addressBooks = new HashMap<>(); // Dictionary to store address books
+    public void createAddressBook() {
+        System.out.println("Enter the name of the new Address Book: ");
+        String addressBookName = sc.nextLine();
 
+        if (addressBooks.containsKey(addressBookName)) {
+            System.out.println("Address Book with name '" + addressBookName + "' already exists.");
+        } else {
+            AddressBook addressBook = new AddressBook();
+            addressBooks.put(addressBookName, addressBook);
+            System.out.println("Address Book '" + addressBookName + "' created.");
+        }
+    }
+
+    public void addPersonToAddressBook() {
+        System.out.println("Enter the name of the Address Book to add a person: ");
+        String addressBookName = sc.nextLine();
+
+        AddressBook addressBook = addressBooks.get(addressBookName);
+        if (addressBook != null) {
+            addressBook.addDetails();
+        } else {
+            System.out.println("Address Book with name '" + addressBookName + "' does not exist.");
+        }
+    }
+
+    public void displayAddressBook(String addressBookName) {
+        AddressBook addressBook = addressBooks.get(addressBookName);
+        if (addressBook != null) {
+            System.out.println("Contacts in Address Book '" + addressBookName + "':");
+            addressBook.displayContacts();
+        } else {
+            System.out.println("Address Book with name '" + addressBookName + "' does not exist.");
+        }
+    }
+
+    public void displayAllAddressBooks() {
+        System.out.println("List of Address Books:");
+        for (String addressBookName : addressBooks.keySet()) {
+            System.out.println("- " + addressBookName);
+        }
+    }
     public void addDetails() {
         System.out.println("Enter the number of persons you want to add: ");
         int count = sc.nextInt();
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -2,13 +2,43 @@ package com.bridgelabz;
 import java.util.Scanner;
 public class AddressBookMain {
     public static void main(String[] args) {
+        AddressBook addressBookSystem = new AddressBook();
         Scanner sc = new Scanner(System.in);
-        AddressBook addressBook = new AddressBook();
 
-        System.out.println("Adding persons to the address book:");
-        addressBook.addDetails();
+        while (true) {
+            System.out.println("Address Book System Menu:");
+            System.out.println("1. Create new Address Book");
+            System.out.println("2. Add person to Address Book");
+            System.out.println("3. Display Address Book");
+            System.out.println("4. Display all Address Books");
+            System.out.println("5. Exit");
+            System.out.println("Enter your choice: ");
+            int choice = sc.nextInt();
+            sc.nextLine(); // Consume the newline character
 
-        System.out.println("Contacts in the address book:");
-        addressBook.displayContacts();
+            switch (choice) {
+                case 1:
+                    addressBookSystem.createAddressBook(); // Create a new AddressBook
+                    break;
+                case 2:
+                    addressBookSystem.addPersonToAddressBook(); // Add a person to an AddressBook
+                    break;
+                case 3:
+                    System.out.println("Enter the name of the Address Book to display: ");
+                    String displayAddressBookName = sc.nextLine();
+                    addressBookSystem.displayAddressBook(displayAddressBookName); // Display an AddressBook
+                    break;
+                case 4:
+                    addressBookSystem.displayAllAddressBooks(); // Display all AddressBooks
+                    break;
+                case 5:
+                    System.out.println("Exiting Address Book System.");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+
+            System.out.println("-----------------------------------");
+        }
     }
 }
